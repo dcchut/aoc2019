3d07984485148cba71467b115748864bcc4a05d9
Robert Usher <266585+dcchut@users.noreply.github.com>
2019-12-10 21:52:06 +1100

    Remove unsafe code, add clarifying comments

--- src/questions/q10.rs
+++ src/questions/q10.rs
@@ -6,13 +6,13 @@ use ordered_float::OrderedFloat;
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug, Clone)]
-pub struct Map {
+pub struct AsteroidMap {
     pub asteroids: HashSet<Position>,
     pub height: usize,
     pub width: usize,
 }
 
-impl Map {
+impl AsteroidMap {
     pub fn new(asteroids: HashSet<Position>, height: usize, width: usize) -> Self {
         Self {
             asteroids,
@@ -33,8 +33,8 @@ impl Map {
     }
 }
 
-impl Extract<Map> for ProblemInput {
-    fn extract(&self) -> Result<Map> {
+impl Extract<AsteroidMap> for ProblemInput {
+    fn extract(&self) -> Result<AsteroidMap> {
         let height = self.lines.len();
         let width = self
             .lines
@@ -55,15 +55,57 @@ impl Extract<Map> for ProblemInput {
             .flatten()
             .collect::<HashSet<_>>();
 
-        Ok(Map::new(asteroids, height, width))
+        Ok(AsteroidMap::new(asteroids, height, width))
     }
+}
+
+fn angle_map(map: &AsteroidMap) -> HashMap<Position, Vec<(Position, f64, i64)>> {
+    // Build up a `HashMap` whose keys are asteroids, and whose values are vectors of triples:
+    // `(other_asteroid, angle, distance)`.
+    let mut by_angle = HashMap::new();
+    for &asteroid in map.asteroids.iter() {
+        let entry = by_angle.entry(asteroid).or_insert_with(Vec::new);
+
+        for &other_asteroid in map.asteroids.iter() {
+            if asteroid == other_asteroid {
+                continue;
+            }
+
+            // compute the angle betwixt the asteroids
+            let angle = ((other_asteroid.y - asteroid.y) as f64)
+                .atan2((other_asteroid.x - asteroid.x) as f64)
+                .to_degrees();
+
+            // compute the distance betwixt the asteroids
+            let distance =
+                (other_asteroid.y - asteroid.y).pow(2) + (other_asteroid.x - asteroid.x).pow(2);
+
+            entry.push((other_asteroid, angle, distance));
+        }
+    }
+
+    // For each point, order its victims starting with an angle of -90.0 and going clockwise.
+    // If two victims share an angle, the tie goes to the closer one.
+    for (_, v) in by_angle.iter_mut() {
+        v.sort_by_key(|(_, ang, dst)| {
+            let of = OrderedFloat(*ang);
+
+            (
+                if of >= OrderedFloat(-90.0) { 0 } else { 1 },
+                OrderedFloat(*ang),
+                *dst,
+            )
+        });
+    }
+
+    by_angle
 }
 
 pub struct Q10;
 
 impl Solution for Q10 {
     fn part1(&self, lines: &ProblemInput) -> i64 {
-        let map: Map = lines.extract().unwrap();
+        let map: AsteroidMap = lines.extract().unwrap();
 
         let n = map.asteroids.len();
         let mut blockers: HashMap<Position, HashSet<Position>> = HashMap::new();
@@ -100,95 +142,47 @@ impl Solution for Q10 {
             .min_by_key(|&(_, count)| count)
             .unwrap();
 
-        dbg!(counts.0);
-
         (n - counts.1 - 1) as i64
     }
 
     fn part2(&self, lines: &ProblemInput) -> i64 {
-        let map: Map = lines.extract().unwrap();
+        let map: AsteroidMap = lines.extract().unwrap();
+        let mut angle_map = angle_map(&map);
 
-        dbg!(map.asteroids.len());
+        // We only care about the point (11, 11).
+        let ordered_points = angle_map
+            .iter_mut()
+            .find(|pos| pos.0.x == 11 && pos.0.y == 11)
+            .unwrap()
+            .1;
 
-        let mut by_angle = HashMap::new();
-
-        for &asteroid in map.asteroids.iter() {
-            let entry = by_angle.entry(asteroid).or_insert_with(Vec::new);
-
-            for &other_asteroid in map.asteroids.iter() {
-                if asteroid == other_asteroid {
-                    continue;
-                }
-
-                // compute the angle betwixt the asteroids
-                let angle = ((other_asteroid.y - asteroid.y) as f64)
-                    .atan2((other_asteroid.x - asteroid.x) as f64)
-                    .to_degrees();
-
-                let distance =
-                    (other_asteroid.y - asteroid.y).pow(2) + (other_asteroid.x - asteroid.x).pow(2); //
-
-                let index = entry
-                    .binary_search_by_key(&OrderedFloat(angle), |(_, ang, _)| OrderedFloat(*ang));
-
-                entry.insert(
-                    match index {
-                        Ok(index) => index,
-                        Err(index) => index,
-                    },
-                    (other_asteroid, angle, distance),
-                );
-            }
-        }
-
-        // Now collect each of these vectors based on min distance
-        // let mut col = Vec::new();
-
-        for (_, kaleidoscope) in by_angle.iter_mut() {
-            kaleidoscope.sort_by_key(|(_, ang, dst)| {
-                let of = OrderedFloat(*ang);
-
-                (
-                    if of >= OrderedFloat(-90.0) { 0 } else { 1 },
-                    OrderedFloat(*ang),
-                    *dst,
-                )
-            });
-        }
-
-        let mut desire = None;
-
-        for pos in by_angle.keys() {
-            if pos.x == 11 && pos.y == 11 {
-                desire = Some(pos);
-            }
-        }
-
-        let kal = by_angle
-            .get_mut(unsafe { (desire.unwrap() as *const Position).as_ref().unwrap() })
-            .unwrap();
-
-        let mut z = 0;
-        while !kal.is_empty() {
+        let mut counter = 0;
+        while !ordered_points.is_empty() {
+            // Keep track of the last angle we saw
             let mut last_angle = OrderedFloat(-1000.0);
             let mut ix = 0;
 
-            while ix < kal.len() {
-                if OrderedFloat(kal[ix].1) != last_angle {
-                    // remove this index from ka?
-                    last_angle = OrderedFloat(kal[ix].1);
+            while ix < ordered_points.len() {
+                let pt = ordered_points[ix];
+                let of = OrderedFloat(pt.1);
 
-                    z += 1;
-                    if z == 200 {
-                        return kal[ix].0.x * 100 + kal[ix].0.y;
+                if of != last_angle {
+                    last_angle = of;
+
+                    // If we've found the 200th laser victim, we're done.
+                    counter += 1;
+                    if counter == 200 {
+                        return pt.0.x * 100 + pt.0.y;
                     }
-                    kal.remove(ix);
+
+                    // Otherwise DESTROY this one.
+                    ordered_points.remove(ix);
                 } else {
+                    // If we haven't found another angle, then we're looking at asteroids hidden behind other asteroids.
                     ix += 1;
                 }
             }
         }
-
         0
     }
 }
